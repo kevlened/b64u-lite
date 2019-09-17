6e992da6e58f1a0027354eaab53c8790db077e12
kevlened <boyettel@gmail.com>
2019-09-17 10:07:14 -0700

    Add buffer toBase64Url and toBuffer

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { atob, btoa, toBase64, fromBase64 } from 'b64-lite'
+import { atob, btoa, toBase64, fromBase64, toBuffer as toBuffer64 } from 'b64-lite'
 
 export function toBinaryString(b64u) {
   return atob(b64u
@@ -13,8 +13,8 @@ export function fromBinaryString(binaryString, padding) {
     .replace(/=/g, padding ? '=' : '')
 }
 
-export function toBase64Url(binaryString, padding) {
-  return toBase64(binaryString)
+export function toBase64Url(input, padding) {
+  return toBase64(input)
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=/g, padding ? '=' : '')
@@ -22,6 +22,12 @@ export function toBase64Url(binaryString, padding) {
 
 export function fromBase64Url(b64u) {
   return fromBase64(b64u
+    .replace(/-/g, '+')
+    .replace(/_/g, '/'))
+}
+
+export function toBuffer(b64u) {
+  return toBuffer64(b64u
     .replace(/-/g, '+')
     .replace(/_/g, '/'))
 }
